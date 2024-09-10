a834f55bf51666a5b9a478a7f29f45edf48e02b8
thiratmaleewong <frank55555552@gmail.com>
2024-09-10 15:15:43 +0700

    “update feature2”

--- Lab09_fantastrawberriesปั่น/Game1.cs
+++ Lab09_fantastrawberriesปั่น/Game1.cs
@@ -44,7 +44,7 @@ namespace Lab09_fantastrawberriesปั่น
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+           //asdasfafs
 
             base.Draw(gameTime);
         }
